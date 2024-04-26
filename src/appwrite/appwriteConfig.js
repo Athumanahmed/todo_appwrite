cb3c3bf77721864ed17ef7b9fc516a6e7bd74e14
athumanahmed <athumanahmed@gmail.com>
2024-04-26 14:10:25 +0300

    initial commit

---
+++ src/appwrite/appwriteConfig.js
@@ -0,0 +1,11 @@
+import {Client, Account, Databases} from 'appwrite'
+
+const client = new Client()
+
+client.setEndpoint('https://cloud.appwrite.io/v1').setProject('662b7bc5e5b5605e798a')
+
+export const account = new Account(client)
+
+// databases
+
+export const databases = new Databases(client , '662b7dda7ca335ad09c8')
---
+++ src/components/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {useState} from 'react'
+import {account} from '../appwrite/appwriteConfig'
+import { useNavigate } from 'react-router-dom'
+import {v4 as uuid} from'uuid'
+function Signup() {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
