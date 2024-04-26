091ba24645b014c140fd3eb73decfa79e0b00120
athumanahmed <athumanahmed@gmail.com>
2024-04-26 17:00:42 +0300

    phase two

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {account} from '../appwrite/appwriteConfig'
+import {useNavigate} from 'react-router-dom'
+
+function Profile() {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
